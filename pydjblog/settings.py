09c579477ae3ca6a604e2ce8c8da26fa6de6c92e
karnika122 <karnika1202@gmail.com>
2020-02-05 12:22:26 -0500

    First Commit

--- pydjblog/settings.py
+++ pydjblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(#3*n4!eifk-7!k_2zc7g%79gz-u%ml5z)gyjd)=hlyp95h667'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['demoit.herokuapp.com']
 
